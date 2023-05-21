e37b66b4a6105ff8a21e65fbb604fb32bfade3f4
EShroomThe1st <xxxthedestroyer058xxx@gmail.com>
2023-05-21 11:34:41 +0700

    Pyrocinical 2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Carousel from './Component/Slider/Slider'
 import './Body.css';
 import FoodInfo from './Component/FoodInfo/FoodInfo.jsx';
 import About from './Component/About/About';
+import Header from './Component/Header/Header';
+import Menu from './Component/menuBar/Menu.jsx';
 
 const App = () => {
   return (
-    <div className='Body'>
-        <div className='InnerBody'>
-            <Carousel/>
-            <FoodInfo/>
-            <About/>
-        </div>
+    <div>
+      <Header/>
+      <Menu/>
+      <div className='Body'>
+          <div className='InnerBody'>
+              <Carousel/>
+              <FoodInfo/>
+              <About/>
+          </div>
+      </div>
     </div>
   )
 }
